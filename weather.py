8e7ca4cc89075fa702c4583912b7132dda2c1bef
degrowenn <145929530+DegrOwenn@users.noreply.github.com>
2024-10-05 17:59:00 +0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,7 +2,8 @@ import requests
 
 def weather(city):
 
-    url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+    API_KEY = "YourAPIKey"
+    url = f'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid={API_KEY}'
     try:
         weather_data = requests.get(url).json()
         temperature = round(weather_data['main']['temp'])
